7cf461473cfe904e99b1ae116a6017f72ee157cf
Geethalakshmi Saravanan <geethalakshmisa13699@gmail.com>
2025-11-28 18:31:34 +0530

    Update PS0009.sql

--- Day-3/SQL/PS0009.sql
+++ Day-3/SQL/PS0009.sql
@@ -1,5 +1,29 @@
 -- N consecutive login days (ROW_NUMBER trick)
 
+DROP TABLE IF EXISTS logins;
+
+CREATE TABLE logins (
+    user_id    INT,
+    login_date DATE
+);
+
+INSERT INTO logins (user_id, login_date) VALUES
+-- user 1: 3 consecutive days (1,2,3) AND another 3 (5,6,7)
+(1, '2025-11-01'),
+(1, '2025-11-02'),
+(1, '2025-11-03'),
+(1, '2025-11-05'),
+(1, '2025-11-06'),
+(1, '2025-11-07'),
+-- user 2: no 3-day streak
+(2, '2025-11-01'),
+(2, '2025-11-03'),
+(2, '2025-11-04'),
+-- user 3: exactly 3 days (10,11,12)
+(3, '2025-11-10'),
+(3, '2025-11-11'),
+(3, '2025-11-12');
+
 -- Find users who have at least 3 consecutive login days. (This is also the pattern you can extend to 4, 5 days, etc.)
 -- Shown for N = 4
 WITH numbered AS (
