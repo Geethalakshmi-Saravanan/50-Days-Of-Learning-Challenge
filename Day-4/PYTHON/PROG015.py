906ee151fa694e94eafd742a25178623b53b941c
Geethalakshmi Saravanan <geethalakshmisa13699@gmail.com>
2025-11-29 17:37:25 +0530

    Update PROG015.py

--- Day-4/PYTHON/PROG015.py
+++ Day-4/PYTHON/PROG015.py
@@ -7,17 +7,16 @@ Find the length of the longest consecutive streak where the number of steps is g
 A streak resets whenever a number falls below 8000.
 
 Sample Input
-steps = [5000, 8200, 9000, 4000, 10000, 11000, 12000]
+steps = [5000, 8200, 9000, 4000, 10000, 11000, 12000, 3000]
 
 Expected Output
 3
 '''
 
+# Approach 1
 steps = [5000, 8200, 9000, 4000, 10000, 11000, 12000, 3000]
-
 current = 0
 max_streak = 0
-
 for s in steps:
     if s >= 8000:
         current += 1
@@ -25,5 +24,13 @@ for s in steps:
             max_streak = current
     else:
         current = 0
+print(max_streak)
 
+# Approach 2
+steps = [5000, 8200, 9000, 4000, 10000, 11000, 12000, 3000]
+current = 0
+max_streak = 0
+for s in steps:
+    current = current + 1 if s >= 8000 else 0
+    max_streak = max(max_streak, current)
 print(max_streak)
