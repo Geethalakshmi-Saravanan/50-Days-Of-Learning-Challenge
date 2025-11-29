e528b375da0216c44b772b95ae8d495b945650c3
Geethalakshmi Saravanan <geethalakshmisa13699@gmail.com>
2025-11-29 17:36:28 +0530

    Create PROG015.py

---
+++ Day-4/PYTHON/PROG015.py
@@ -0,0 +1,29 @@
+'''
+Longest Consecutive Streak Above Threshold
+
+Question
+You are given a list of daily step counts.
+Find the length of the longest consecutive streak where the number of steps is greater than or equal to 8000.
+A streak resets whenever a number falls below 8000.
+
+Sample Input
+steps = [5000, 8200, 9000, 4000, 10000, 11000, 12000]
+
+Expected Output
+3
+'''
+
+steps = [5000, 8200, 9000, 4000, 10000, 11000, 12000, 3000]
+
+current = 0
+max_streak = 0
+
+for s in steps:
+    if s >= 8000:
+        current += 1
+        if current > max_streak:
+            max_streak = current
+    else:
+        current = 0
+
+print(max_streak)
